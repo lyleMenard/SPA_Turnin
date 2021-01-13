d9120d1f6421d40b8eca45435f4a579b16912c9b
Lyle Menard <lylemenard833@gmail.com>
2021-01-13 11:00:45 -0600

    added and implemented mouse

---
+++ assignment_1/src/mouse.java
@@ -0,0 +1,41 @@
+package src;
+
+import java.util.ArrayList;
+
+public class mouse {
+    public String name;
+    public Integer xCoordinate;
+    public Integer yCoordinate;
+    public ArrayList<Integer[]> coordinateHistoryStack;
+    public String getMouseName() {
+        return "null";
+    }
+
+    public Integer getxCoordinate() {
+        return 0;
+    }
+
+    public Integer getyCoordinate() {
+        return 0;
+    }
+
+    public void printInformation() {
+
+    }
+
+    public void moveUp() {
+
+    }
+
+    public void moveDown() {
+
+    }
+
+    public void moveRight() {
+
+    }
+
+    public void moveLeft() {
+
+    }
+}
